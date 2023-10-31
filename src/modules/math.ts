de99baadbeaab30ae12dcc970e938e31b3c25d7c
MoritzRS <moritz.r.schulz@gmail.com>
2023-10-31 12:45:16 +0100

    added numeric comparison

--- src/modules/math.ts
+++ src/modules/math.ts
@@ -2,6 +2,7 @@ import { Module } from "../module";
 
 export const math: Module = {
 	variables: {
+		// Calculations
 		"+": (...values: number[]) => values.reduce((a, b) => a + b),
 		"-": (...values: number[]) => values.reduce((a, b) => a - b),
 		"*": (...values: number[]) => values.reduce((a, b) => a * b),
@@ -15,6 +16,40 @@ export const math: Module = {
 		round: (value: number) => Math.round(value),
 		floor: (value: number) => Math.floor(value),
 		ceil: (value: number) => Math.ceil(value),
+
+		// Comparison
+		"<": (current: number, ...values: number[]) => {
+			if (!values?.length) return true;
+			for (const value of values) {
+				if (current >= value) return false;
+				current = value;
+			}
+			return true;
+		},
+		"<=": (current: number, ...values: number[]) => {
+			if (!values?.length) return true;
+			for (const value of values) {
+				if (current > value) return false;
+				current = value;
+			}
+			return true;
+		},
+		">": (current: number, ...values: number[]) => {
+			if (!values?.length) return true;
+			for (const value of values) {
+				if (current <= value) return false;
+				current = value;
+			}
+			return true;
+		},
+		">=": (current: number, ...values: number[]) => {
+			if (!values?.length) return true;
+			for (const value of values) {
+				if (current < value) return false;
+				current = value;
+			}
+			return true;
+		},
 	},
 	constants: {
 		PI: Math.PI,
